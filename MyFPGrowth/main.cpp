23aa3027e2488768bd06c9d80d4ddac8cbf02bb2
thatcharles <charles5j21z@gmail.com>
2018-03-25 21:20:29 +0800

    Initial Commit

---
+++ MyFPGrowth/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyFPGrowth
+//
+//  Created by CharlesChung on 2018/3/25.
+//  Copyright © 2018年 CharlesChung. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
